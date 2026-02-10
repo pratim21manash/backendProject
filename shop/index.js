b16dd7c83c2f5761681419339b642db68025609b
manash <pratimkashyapm@gmail.com>
2026-02-10 12:19:00 +0530

    server created

---
+++ shop/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8080
+
+app.listen(PORT, () => {
+    console.log("Server is started on port", PORT)
+})

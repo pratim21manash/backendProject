c1ab76830863c4e8b60a23e45d06ead2477aea2a
manash <pratimkashyapm@gmail.com>
2026-02-10 13:37:07 +0530

    userModel

--- shop/index.js
+++ shop/index.js
@@ -1,8 +1,19 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+
 dotenv.config();
 
 const app = express();
+
+mongoose.connect(process.env.DB)
+  .then(()=>{
+    console.log("Database connected")
+  })
+
+  .catch((err)=>{
+    console.log("Database connection failed:", err.message)
+  })
 
 const PORT = process.env.PORT || 8080
 
---
+++ shop/model/user.model.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose")
+const bcrypt = require("bcrypt")
+
+const userSchema = new Schema(
+    {
+        fullName: {
+            type: String,
+            trim: true,
+            required: true
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: true,
+            unique: true,
+            lowercase: true
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        role: {
+            type: String,
+            enum: ["admin", "manager", "staff"],
+            default: "staff"
+        },
+        storeName: {
+            type: String,
+            trim: true,
+            default: "My Store"
+        }
+    },
+    { timestamps: true }
+);
+
+userSchema.pre("save", async function(next){
+    const count = await model("User").countDocuments({email: this.email})
+
+    if(count > 0)
+        throw next(new Error("Email already exist"))
+
+    const encryptedPassword = await bcrypt.hash(this.password.toString(), 12)
+    this.password = encryptedPassword
+    next()
+})
+
+
+const UserModel = model("User", userSchema);
+module.exports = UserModel
